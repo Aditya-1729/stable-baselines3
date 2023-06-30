602ab4a15ee7e73de1a91918cf80ed2ef6c09823
aditya.pradhan <aditya.pradhan@rwth-aachen.de>
2023-06-30 17:05:39 +0200

    added RL experiment file

--- sac_robosuite.py
+++ sac_robosuite.py
@@ -1,43 +1,38 @@
 import gymnasium as gym
 import robosuite as suite
-from robosuite.wrappers import GymWrapper
+from robosuite.wrappers import RL_agent_2
 from stable_baselines3 import SAC
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.callbacks import EvalCallback
 from robosuite.controllers import load_controller_config
+from omegaconf import DictConfig, OmegaConf
+from hydra import compose, initialize_config_dir
 
-env = GymWrapper(
-        suite.make(
-            "Wipe",
-            robots="Panda",
-            controller_configs= load_controller_config(default_controller="OSC_POSE"),  # use Sawyer robot
-            use_camera_obs=False,  # do not use pixel observations
-            has_offscreen_renderer=False,  # not needed since not using pixel obs
-            has_renderer=False,  # make sure we can render to the screen
-            reward_shaping=True,  # use dense rewards
-            control_freq=20,  # control should happen fast enough so that simulation looks smooth
-        )
-    )
+initialize_config_dir(version_base=None, config_dir="/media/aditya/OS/Users/Aditya/Documents/Uni_Studies/Thesis/master_thesis/git_clean/robosuite/robosuite/main/config")
+cfg = compose(config_name="main")
+
+env = RL_agent_2(suite.make(env_name=cfg.env.name,
+                **cfg.env.specs,
+                task_config = OmegaConf.to_container(cfg.task_config),
+                controller_configs=OmegaConf.to_container(cfg.controller)), cfg
+)
+
 
 eval_env = env
 # Use deterministic actions for evaluation
 
-
 # env = gym.make('Pendulum-v1')
 # new_logger = configure(["stdout", "csv", "tensorboard"])
-model = SAC("MlpPolicy", env, verbose=1, tensorboard_log="./sac_train_eval/",
-            learning_starts=3300, batch_size=128, target_update_interval=5,
-             train_freq=(1,"step"), tau=0.005, )
-# env = model.get_env()[0]
+model = SAC(env=env, **cfg.algorithm.model)
 
-eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/',
-                             log_path='./logs/', eval_freq=10000, 
-                             deterministic=True, render=False, n_eval_episodes=5)
+# eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/',
+#                              log_path='./logs/', eval_freq=10000, 
+#                              deterministic=True, render=False, n_eval_episodes=5)
 
 
 
-# # model.set_logger(new_logger)
-model.learn(total_timesteps=10000, log_interval=2, callback=eval_callback)
+# # # model.set_logger(new_logger)
+model.learn(**cfg.algorithm.learn)
 # model.save("sac_robosuite_2")
 
 # del model # remove to demonstrate saving and loading
