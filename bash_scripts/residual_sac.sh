09b26ad22811a8318de5ca6e731e9e69ab2e9878
emma.pabich <emma.cramer@dsme.rwth-aachen.de>
2023-12-05 22:06:02 +0100

    Updated path and environment to ec account

--- bash_scripts/residual_sac.sh
+++ bash_scripts/residual_sac.sh
@@ -1,35 +1,34 @@
 #!/usr/local_rwth/bin/zsh
 
-#SBATCH --job-name=Res_37
-#SBATCH --output=/work/thes1499/DR_19_10/robosuite/stable-baselines3/task_out/task_out.%J.out
-#SBATCH --error=/work/thes1499/DR_19_10/robosuite/stable-baselines3/error_task/error_task_out.%J.out
+#SBATCH --job-name=ep-env-test
+#SBATCH --output=/home/ep652816/grinding_robot/task_out/task_out.%J.out
+#SBATCH --error=/home/ep652816/grinding_robot/error_task/error_task_out.%J.out
 #SBATCH --nodes=1 # request one nodes
 #SBATCH --cpus-per-task=8  # ask for 2 cpus per task
 #SBATCH --mem=64G
-#SBATCH --account=rwth1458
-#SBATCH --time=17:00:00
-# SBATCH --gres=gpu:volta:1
+#SBATCH --time=00:05:00
+#SBATCH --gres=gpu:volta:1
 
 # request one gpu per node 
 
 
+module load GCCcore/.9.3.0
+module load cuDNN/8.6.0.163-CUDA-11.8.0
+
 export CONDA_ROOT=$HOME/miniconda3
-. $CONDA_ROOT/etc/profile.d/conda.sh
+source $CONDA_ROOT/etc/profile.d/conda.sh
 export PATH="$CONDA_ROOT/bin:$PATH"
 
-
-# load modules
-# module switch intel gcc      
 conda activate robosuite
 
 # PYTHONPATH='/hpcwork/ru745256/master_thesis/robosuite-benchmark/rlkit':${PYTHONPATH}
 # PYTHONPATH='/hpcwork/ru745256/master_thesis/robosuite-benchmark':${PYTHONPATH}
-PYTHONPATH='/work/thes1499/DR_19_10/robosuite':${PYTHONPATH}
-PYTHONPATH='/work/thes1499/DR_19_10/robosuite/stable-baselines3':${PYTHONPATH}                       
+PYTHONPATH='/home/ep652816/grinding_robot/robosuite/stable-baselines3':${PYTHONPATH}
+PYTHONPATH='/home/ep652816/grinding_robot/robosuite':${PYTHONPATH}                       
 export PYTHONPATH
 export MUJOCO_GL='disabled'
 #export PYOPENGL_PLATFORM=osmesa
 #export DISPLAY=guilinuxbox:0.0
 
 
-python /work/thes1499/DR_19_10/robosuite/stable-baselines3/Residual_RL/examples/residual.py algorithm=sac_residual task_config=force_control_4 controller.agent_config=residual_2 experiment=sac_residual seed=37
+python /home/ep652816/grinding_robot/robosuite/stable-baselines3/Residual_RL/examples/residual.py algorithm=sac_residual task_config=force_control_4 controller.agent_config=residual_2 experiment=sac_residual seed=37
