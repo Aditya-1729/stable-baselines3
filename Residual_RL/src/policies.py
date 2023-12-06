ad618421999468ae209d7ac379a072bd62dad5f5
Emma Cramer <emma.pabich@dsme.rwth-aachen.de>
2023-12-06 07:39:25 +0100

    don log ent value

--- Residual_RL/src/policies.py
+++ Residual_RL/src/policies.py
@@ -307,7 +307,7 @@ class ResidualSAC(SAC):
         self.logger.record("train/ent_coef", np.mean(ent_coefs))
         self.logger.record("train/actor_loss", np.mean(actor_losses))
         self.logger.record("train/critic_loss", np.mean(critic_losses))
-        self.logger.record("train/ent_value", np.linalg.norm(ent_value)) # added to log entropy
+        #self.logger.record("train/ent_value", np.linalg.norm(ent_value)) # added to log entropy
         if len(ent_coef_losses) > 0:
             self.logger.record("train/ent_coef_loss", np.mean(ent_coef_losses))
     
